c363e8671428a1a885b1e24937005bf35cbbef96
viewer <2658118740@qq.com>
2025-03-19 15:31:49 +0800

    oj项目开工

---
+++ oj-modules/oj-system/src/main/java/com/viewer/system/OjSystemApplication.java
@@ -0,0 +1,10 @@
+package com.viewer.system;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OjSystemApplication.class, args);
+    }
+}
