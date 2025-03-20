61d7d70804b637e1025ae1a28ccaf90dc63fadac
viewer <2658118740@qq.com>
2025-03-20 21:41:14 +0800

    添加统一返回结果

---
+++ oj-common/oj-common-core/src/main/java/com/viewer/common/core/domain/Result.java
@@ -0,0 +1,29 @@
+package com.viewer.common.core.domain;
+
+import lombok.Data;
+
+@Data
+public class Result<T> {
+    private int code;
+    private String errMeg;
+    private T data;
+
+    public static <T> Result fail(Integer code, String message){
+        Result<T> result = new Result<>();
+
+        result.setCode(code);
+        result.setErrMeg(message);
+
+        return result;
+    }
+
+    public static <T> Result fail(Integer code, String message, T data){
+        Result<T> result = new Result<>();
+
+        result.setCode(code);
+        result.setErrMeg(message);
+        result.setData(data);
+
+        return result;
+    }
+}
