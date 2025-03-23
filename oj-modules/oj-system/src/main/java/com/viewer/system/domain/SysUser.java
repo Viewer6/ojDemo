ae3d2aff4192bfef94cb7bf0ba46f6ab62bf87a2
viewer <2658118740@qq.com>
2025-03-23 13:22:58 +0800

    对redis测试

--- oj-modules/oj-system/src/main/java/com/viewer/system/domain/SysUser.java
+++ oj-modules/oj-system/src/main/java/com/viewer/system/domain/SysUser.java
@@ -5,8 +5,10 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.viewer.common.core.domain.BaseEntity;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 @TableName("tb_sys_user")
 public class SysUser extends BaseEntity {
 
--- oj-modules/oj-system/src/main/java/com/viewer/system/test/controller/TestController.java
+++ oj-modules/oj-system/src/main/java/com/viewer/system/test/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.viewer.system.test.controller;
 
+import com.viewer.common.redis.service.RedisService;
+import com.viewer.system.domain.SysUser;
 import com.viewer.system.test.service.ITestService;
 import com.viewer.system.test.service.impl.TestServiceImpl;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +14,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/test")
 public class TestController {
-
     @Resource(name = "testServiceImpl")
     private TestServiceImpl testService;
+
+    @Resource
+    private RedisService redisService;
 
     @RequestMapping("/list")
     public List<?> list(){
         return testService.list();
+    }
+
+    @RequestMapping("/redisTest")
+    public String redisTest(){
+        SysUser sysUser = new SysUser();
+        sysUser.setUserAccount("redisTest");
+        redisService.setCacheObject("user", sysUser);
+
+        SysUser us = redisService.getCacheObject("user", SysUser.class);
+        return us.toString();
     }
 }
