230259fadb16b8d5257c2c0484f9a9141a7f0e98
viewer <2658118740@qq.com>
2025-04-25 23:41:22 +0800

    修复向前端传的题目id被截断bug

--- oj-modules/oj-system/src/main/java/com/viewer/system/domain/question/vo/QuestionDetailVO.java
+++ oj-modules/oj-system/src/main/java/com/viewer/system/domain/question/vo/QuestionDetailVO.java
@@ -1,10 +1,14 @@
 package com.viewer.system.domain.question.vo;
 
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 @Data
 public class QuestionDetailVO {
+
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long questionId;
 
     private String title;
--- oj-modules/oj-system/src/main/java/com/viewer/system/domain/question/vo/QuestionListVO.java
+++ oj-modules/oj-system/src/main/java/com/viewer/system/domain/question/vo/QuestionListVO.java
@@ -1,11 +1,15 @@
 package com.viewer.system.domain.question.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class QuestionListVO {
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long questionId;
 
     private String title;
@@ -14,5 +18,6 @@ public class QuestionListVO {
 
     private String createName;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
 }
