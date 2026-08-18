6c68be1468675775269441502bcd4a92c6e0b9e4
viewer <2658118740@qq.com>
2026-08-18 21:26:27 +0800

    refactor: 代码格式化，清理无效导入与废弃代码

--- oj-modules/oj-friend/src/main/java/com/viewer/friend/OjFriendApplication.java
+++ oj-modules/oj-friend/src/main/java/com/viewer/friend/OjFriendApplication.java
@@ -1,6 +1,9 @@
+package com.viewer.friend;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @MapperScan("com.viewer.**.mapper")
