366757680a16b5a8d444929bef38f684b1e82ef3
viewer <2658118740@qq.com>
2026-08-18 21:24:34 +0800

    feat: 阿里云短信验证测试

--- oj-modules/oj-friend/src/main/java/com/viewer/friend/test/controller/TestController.java
+++ oj-modules/oj-friend/src/main/java/com/viewer/friend/test/controller/TestController.java
@@ -1,4 +1,26 @@
 package com.viewer.friend.test.controller;
 
-public class TestController {
+
+import com.viewer.common.core.controller.BaseController;
+import com.viewer.common.core.domain.Result;
+import com.viewer.common.message.service.AliSmsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController extends BaseController {
+
+    @Autowired
+    private AliSmsService aliSmsService;
+
+    @GetMapping("/sendCode")
+    public Result<Void> sendCode(String phone, String code) {
+        log.info("验证码发送测试, {}, {}", phone, code);
+        return getResult(aliSmsService.sendMobileCode(phone, code));
+    }
 }
